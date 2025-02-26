48fe64291a1efba7289f9c7aef98e5b50b1353fa
Manuel Filgueira Marín <manuelfilgueira@gmail.com>
2025-02-26 12:37:31 -0300

    instalacion de paquetes y dependencias terminadas

--- frontend/LandingPage.js
+++ frontend/LandingPage.js
@@ -1,23 +1,16 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing Page</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header>
-        <nav id="navbar">
-            </nav>
-    </header>
 
-    <main id="body">
-        </main>
+// <body>
+//     <header>
+//         <nav id="navbar">
+//             </nav>
+//     </header>
 
-    <footer id="footer">
-        </footer>
+//     <main id="body">
+//         </main>
 
-    <script src="script.js"></script>
-</body>
-</html>
+//     <footer id="footer">
+//         </footer>
+
+//     <script src="script.js"></script>
+// </body>
+// </html>
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
-function Ping() {
-  return <h1>Pong</h1>;
-}
+// function Ping() {
+//   return <h1>Pong</h1>;
+// }
 
 function App() {
   const [count, setCount] = useState(0)
