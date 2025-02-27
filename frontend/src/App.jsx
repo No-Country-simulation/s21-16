598e2961f05d0677ca737dc9b53ee185c40c3b97
fhdzleon <fhdzleon@gmail.com>
2025-02-27 10:14:10 -0600

    refactor(landingpage) add template for components of landingpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,15 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
- 
-import HeroSection from "./components/HeroSection/HeroSection";
-import Functionalities from "./components/Functionalities/Functionalities";
-import Beneffits from "./components/Benefits/Benefits";
-import MenuDemo from "./components/MenuDemo/MenuDemo";
-import Menu from './components/MenuDemo/Menu/Menu'; 
-import Reviews from "./components/Reviews/Reviews";
-import CreateMenu from './components/HeroSection/CreateMenu/CreateMenu';
-import './App.css';
- 
- 
- 
+import LandingPage from "./templates/LandingPage.tsx";
+import Menu from "./components/MenuDemo/Menu/Menu";
+import CreateMenu from "./components/HeroSection/CreateMenu/CreateMenu";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <HeroSection />
-              <Functionalities />
-              <Beneffits />
-              <MenuDemo />
-              <Reviews />
-            </>
-          }
-        />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/menuDemo" element={<Menu />} />
         <Route path="/creamenu" element={<CreateMenu />} />
       </Routes>
---
+++ frontend/src/templates/LandingPage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import HeroSection from "../components/HeroSection/HeroSection";
+import Benefits from "../components/Benefits/Benefits";
+import Functionalities from "../components/Functionalities/Functionalities";
+import MenuDemo from "../components/MenuDemo/MenuDemo";
+import Reviews from "../components/Reviews/Reviews";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <HeroSection />
+      <Functionalities />
+      <Benefits />
+      <MenuDemo />
+      <Reviews />
+    </div>
+  );
+};
+
+export default LandingPage;
