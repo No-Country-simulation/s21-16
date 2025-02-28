07232109edca62e0273e2d4cae57f4b2c65a542b
fhdzleon <fhdzleon@gmail.com>
2025-02-28 09:41:03 -0600

    refacto(navbar)Fix Lint errors, add button to login in navbar, add smooth scrolling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,21 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import './App.css'
-
-import Menu from './components/MenuDemo/Menu/Menu'; 
-
-import CreateMenu from './components/HeroSection/CreateMenu/CreateMenu';
-import './App.css';
-
-import LandingPage from './templates/LandingPage';
+import "./App.css";
+import Menu from "./components/MenuDemo/Menu/Menu";
+import CreateMenu from "./components/HeroSection/CreateMenu/CreateMenu";
+import "./App.css";
+import LandingPage from "./templates/LandingPage";
+import Signin from "./templates/Signin";
+import Signup from "./templates/Signup";
 
 function App() {
- 
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={ <LandingPage/> } />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/menuDemo" element={<Menu />} />
         <Route path="/creamenu" element={<CreateMenu />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
--- frontend/src/templates/LandingPage.jsx
+++ frontend/src/templates/LandingPage.jsx
@@ -3,18 +3,28 @@ import Benefits from "../components/Benefits/Benefits";
 import Functionalities from "../components/Functionalities/Functionalities";
 import MenuDemo from "../components/MenuDemo/MenuDemo";
 import Reviews from "../components/Reviews/Reviews";
-import Navbar from "../components/Navbar/Navbar"
-import Footer from "../components/Footer/Footer"
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const LandingPage = () => {
   return (
     <div>
-      <Navbar/>
-      <HeroSection />
-      <Functionalities />
-      <Benefits />
-      <MenuDemo />
-      <Reviews />
+      <Navbar />
+      <div id="inicio">
+        <HeroSection />
+      </div>
+      <div id="Functionalities">
+        <Functionalities />
+      </div>
+      <div id="Beneffits">
+        <Benefits />
+      </div>
+      <div id="menu-demo">
+        <MenuDemo />
+      </div>
+      <div id="contacto">
+        <Reviews />
+      </div>
       <Footer />
     </div>
   );
