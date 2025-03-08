3b0e7bee5a93e85bc805135b472f9fe823b8a8b0
Esteban Durante <estebandurante194@gmail.com>
2025-03-08 20:16:44 -0300

    bugfix(crud-business) cambiada contraseña en aplication properties

--- backend/menuproject/src/main/java/com/menuproject/menuproject/service/menu/MenuServiceImpl.java
+++ backend/menuproject/src/main/java/com/menuproject/menuproject/service/menu/MenuServiceImpl.java
@@ -11,13 +11,12 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class MenuServiceImpl implements ImenuService{
-
+public class MenuServiceImpl implements ImenuService {
 
     private final MenuRepository menuRepository;
     private final BusinessServiceImpl businessServiceImpl;
 
-    public MenuServiceImpl(MenuRepository menuRepository, BusinessServiceImpl businessServiceImpl){
+    public MenuServiceImpl(MenuRepository menuRepository, BusinessServiceImpl businessServiceImpl) {
         this.menuRepository = menuRepository;
         this.businessServiceImpl = businessServiceImpl;
     }
@@ -27,7 +26,7 @@ public class MenuServiceImpl implements ImenuService{
 
         Menu menu = new Menu();
 
-        //verificamos la existencia del negocio
+        // verificamos la existencia del negocio
         Business business = businessServiceImpl.findById(menuRequestDto.businessId());
 
         menu.setName(menuRequestDto.name());
@@ -47,34 +46,35 @@ public class MenuServiceImpl implements ImenuService{
     }
 
     @Override
-    public void upDateMenu(Long idMenu,MenuRequestDto menuRequestDto) {
-        //validamos menu y business
+    public void upDateMenu(Long idMenu, MenuRequestDto menuRequestDto) {
+        // validamos menu y business
         Menu menu;
         Business business;
 
-        menu = menuRepository.findById(idMenu).orElseThrow(()-> new RuntimeException("No se encontro el menu id:"+idMenu));
+        menu = menuRepository.findById(idMenu)
+                .orElseThrow(() -> new RuntimeException("No se encontro el menu id:" + idMenu));
 
         business = businessServiceImpl.findById(menuRequestDto.businessId());
 
-        if(menu != null && business != null){
-            //actualizamos datos
+        if (menu != null && business != null) {
+            // actualizamos datos
             menu.setName(menuRequestDto.name());
             menu.setIdBusiness(business);
 
-            //guardamos datos
+            // guardamos datos
             menuRepository.save(menu);
-        }else {
-            throw new RuntimeException("No se pudo acutalizar");
+        } else {
+            throw new RuntimeException("No se pudo actualizar");
         }
     }
 
     @Override
     public void deleteMenu(Long idMenu) {
-        //validamos si el menu existe
-        Menu menu = menuRepository.findById(idMenu).orElseThrow(()->new RuntimeException("No se encontro el Menu id:" +idMenu));
-        //borramos
+        // validamos si el menu existe
+        Menu menu = menuRepository.findById(idMenu)
+                .orElseThrow(() -> new RuntimeException("No se encontro el Menu id:" + idMenu));
+        // borramos
         menuRepository.delete(menu);
     }
-
 
 }
